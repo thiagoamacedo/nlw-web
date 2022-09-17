210ecb2ee74afc84270b970dca753b9e9c58cab2
Thiago Araujo <thiago_amacedo@hotmail.com>
2022-09-17 07:03:43 -0300

    last additions of project

--- src/components/CreateAdBanner.tsx
+++ src/components/CreateAdBanner.tsx
@@ -1,7 +1,13 @@
 import { MagnifyingGlassPlus } from 'phosphor-react'
 import * as Dialog from '@radix-ui/react-dialog';
+import { App } from '../App'
 
-export function CreateAdBanner() {
+export function CreateAdBanner ({ state, setState }: any) {
+
+  function handleDialogOpen() { 
+    setState(true)
+  }
+
   return (
   <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
     <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
@@ -9,10 +15,10 @@ export function CreateAdBanner() {
         <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
         <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
       </div>
-      <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+      <button onClick={() => handleDialogOpen()} className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
         <MagnifyingGlassPlus size={24}/>
         Publicar anúncio
-      </Dialog.Trigger>
+      </button>
     </div>
   </div>
   )
